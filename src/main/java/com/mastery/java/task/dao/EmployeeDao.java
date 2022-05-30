0851446fa46bd6644a751bd1fa73efdad0d6d4a7
Piotr Ludynia <pietrekpl18@gmail.com>
2022-05-30 21:29:18 +0200

    Complete EmployeeDao by creating CRUD methods

--- src/main/java/com/mastery/java/task/dao/EmployeeDao.java
+++ src/main/java/com/mastery/java/task/dao/EmployeeDao.java
@@ -1,4 +1,19 @@
 package com.mastery.java.task.dao;
 
-public class EmployeeDao {
+import com.mastery.java.task.dto.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+    void insertEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Employee getEmployeeByID(Long employeeID);
+
+    void updateEmployeeById(Long employeeId);
+
+    void deleteEmployeeById(Long employeeId);
+
 }
--- src/main/java/com/mastery/java/task/service/EmployeeService.java
+++ src/main/java/com/mastery/java/task/service/EmployeeService.java
@@ -1,4 +1,10 @@
 package com.mastery.java.task.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+
+
 }
