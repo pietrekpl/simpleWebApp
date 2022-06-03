3ce090f6e072b7abdd685922fc19f2a669dba12c
Piotr Ludynia <pietrekpl18@gmail.com>
2022-06-03 19:12:22 +0200

    fix database connection, add methods in controller for getting all employees and single employee

--- src/main/java/com/mastery/java/task/rest/EmployeeController.java
+++ src/main/java/com/mastery/java/task/rest/EmployeeController.java
@@ -1,4 +1,33 @@
 package com.mastery.java.task.rest;
 
+
+import com.mastery.java.task.dao.EmployeeDao;
+import com.mastery.java.task.dto.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    private EmployeeDao employeeDao;
+
+    public EmployeeController(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @PostMapping("/add")
+    public void addEmployee(@RequestBody Employee employee) {
+        employeeDao.addEmployee(employee);
+    }
+
+    @GetMapping("/all")
+    public List<Employee> findAll() {
+        return employeeDao.findAll();
+    }
 }
+
+
+
+
--- src/main/java/com/mastery/java/task/service/EmployeeService.java
+++ src/main/java/com/mastery/java/task/service/EmployeeService.java
@@ -1,10 +1,15 @@
 package com.mastery.java.task.service;
 
 
+import com.mastery.java.task.dao.EmployeeDao;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 
+    private EmployeeDao employeeDao;
 
+    public EmployeeService(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
 }
