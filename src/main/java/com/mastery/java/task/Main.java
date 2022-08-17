521cde8f0d599a3a90150c1610b0e5e478cc0b9a
p.ludynia <p.ludynia@godeltech.com>
2022-08-17 14:12:53 +0300

    make documentation for a method get, delete, post

--- src/main/java/com/mastery/java/task/Main.java
+++ src/main/java/com/mastery/java/task/Main.java
@@ -1,13 +1,14 @@
 package com.mastery.java.task;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
 @SpringBootApplication
-@OpenAPIDefinition
 public class Main {
 
     public static void main(String[] args) {
--- src/main/java/com/mastery/java/task/controller/EmployeeController.java
+++ src/main/java/com/mastery/java/task/controller/EmployeeController.java
@@ -5,8 +5,15 @@ import com.mastery.java.task.exception.ResourceNotFoundException;
 import com.mastery.java.task.model.Employee;
 import com.mastery.java.task.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +24,7 @@ import java.util.List;
 @RequestMapping("/employees")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "employees", description = "Add / Get / Update / Delete employee")
 public class EmployeeController {
 
 
@@ -24,15 +32,51 @@ public class EmployeeController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    @Operation(summary = "returns all employees or filter employees based on firstName or lastName param")
+    @Operation(summary = "Returns all employees or filter employees based on firstName or lastName param",
+            description = "When no parameter is applied or no firstName/lastName are found, list of all employees is being returned",
+            responses = {@ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(examples = @ExampleObject(summary = "Example of output:", value = """
+                            [
+                              {
+                                  "employeeId": 1,
+                                   "firstName": "James",
+                                   "lastName": "Bond",
+                                   "departmentId": 7,
+                                    "jobTitle": "Agent"
+                                 },
+                              {
+                                  "employeeId": 2,
+                                  "firstName": "Donald",
+                                  "lastName": "Trump",
+                                  "departmentId": 5,
+                                  "jobTitle": "President"
+                              }
+                            ]
+                            """)))},
+            parameters = {@Parameter(name = "firstName", description = "Employee first name (Optional)"),
+                    @Parameter(name = "lastName", description = "Employee last name (Optional)")}
+    )
     public List<Employee> getFilteredEmployeesOrAllEmployees(@RequestParam(value = "firstName", required = false) String firstName,
                                                              @RequestParam(value = "lastName", required = false) String lastName) {
         return employeeService.filterEmployeesByFirstNameOrLastName(firstName, lastName);
     }
 
+
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public Employee getEmployee(@PathVariable("id") Long id, ResourceNotFoundException exception) {
+    @Operation(summary = "Get Employee by Id",
+            description = "Gets single employee by Id",
+            responses = {@ApiResponse(description = "Success", responseCode = "200", content = @Content(examples = @ExampleObject(summary = "Example of output:", value = """
+                      {
+                          "employeeId": 1,
+                           "firstName": "James",
+                           "lastName": "Bond",
+                           "departmentId": 7,
+                            "jobTitle": "Agent"
+                         }
+                    """))),
+                    @ApiResponse(description = "Employee with requested Id not found", responseCode = "404", content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class)))})
+    public Employee getEmployee(@Parameter(name = "id", description = "Employee ID", required = true) @PathVariable("id") Long id) {
         log.info("Method getEmployee() takes id = {}", id);
         return employeeService.getEmployeeById(id);
     }
@@ -40,6 +84,20 @@ public class EmployeeController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @Operation(summary = "Add new employee", description = "Add new Employee",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "The request body is expected to provide the new employee." +
+                    " The `employeeId` could be omitted, and will be accessible even if not specified.", content = @Content(
+                    examples = @ExampleObject(description = "Provides an example `employee` request body to save:",
+                            value = """
+                                      {
+                                           "firstName": "James",
+                                           "lastName": "Bond",
+                                           "departmentId": 7,
+                                            "jobTitle": "Agent"
+                                         }
+                                    """
+                    )
+            )))
     public void addEmployee(@RequestBody() Employee employee) {
         employeeService.save(employee);
     }
@@ -54,7 +112,11 @@ public class EmployeeController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteEmployee(@PathVariable("id") Long id) {
+    @Operation(summary = "Delete employee by provided ID",
+            description = "Delete single employee by provided Id",
+            responses = {@ApiResponse(description = "Successfully deleted", responseCode = "204"),
+                    @ApiResponse(description = "Employee with requested Id not found", responseCode = "404", content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class)))})
+    public void deleteEmployee(@Parameter(name = "id", description = "Employee ID", required = true) @PathVariable("id") Long id) {
         log.info("Method deleteEmployee() takes id = {}", id);
         employeeService.deleteEmployeeById(id);
     }
