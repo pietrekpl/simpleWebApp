fb85a1537605962c5598fccc14c9aaa64141f8e1
p.ludynia <p.ludynia@godeltech.com>
2022-09-14 13:10:15 +0200

    update generated value strategy to identity, liquibase db type change from int to serial

--- src/main/java/com/mastery/java/task/model/Employee.java
+++ src/main/java/com/mastery/java/task/model/Employee.java
@@ -28,8 +28,7 @@ public class Employee implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @SequenceGenerator(name = "seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employee_id")
     private Long employeeId;
 
