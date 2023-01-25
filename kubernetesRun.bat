0dd08e5b6d34b23956c3797fa3ca50aa6238f5fd
p.ludynia <p.ludynia@godeltech.com>
2023-01-25 10:20:18 +0100

    update script to do automatic port-forwarding to run kubernetes services on localhost

--- kubernetesRun.bat
+++ kubernetesRun.bat
@@ -2,3 +2,18 @@ kubectl apply -f allAppDeployment.yaml
 
 kubectl apply -f allServices.yaml
 
+@echo off
+@timeout /t 15
+
+
+start "App" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoExit "-Command" "kubectl port-forward services/app 8080:8080"
+
+start "Angular" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoExit "-Command" "kubectl port-forward services/angular 4200:4200"
+
+start "ActiveMQ" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoExit "-Command" "kubectl port-forward services/activemq 8161:8161"
+
+start "Postgres" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoExit "-Command" "kubectl port-forward services/postgres 5432:5432"
+
+
+
+
